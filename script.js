f9dbdb37186ba328db16f7fd62670b8980a38996
Nicolas Lardier <nicolas.lardier@viacesi.fr>
2020-10-07 22:39:17 +0200

    Création de lobjet snake

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
 window.onload = function (){
 
-    var canvas;
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSize = 30;
     var ctx;
-    var delay = 1000;
+    var delay = 100;
+    var snakee;
+
+    init();
 
     function init() {
-        canvas = document.createElement('canvas');
-        canvas.width = 900;
-        canvas.height= 600;
+        var canvas = document.createElement('canvas');
+        canvas.width = canvasWidth;
+        canvas.height= canvasHeight;
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        snakee = new Snake([[6,4], [5,4], [4,4]]);
         refreshCanvas();
     }
 
     function refreshCanvas() {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight)
         ctx.fillStyle = "#ff0000";
-        ctx.fillRect(30,30, 100, 50);
+        ctx.fillRect(xCoord, yCoord, 100, 50);
+
+        snakee.draw();
+        setTimeout(refreshCanvas, delay);
     }
+
+    function drawBlock(ctx, position){
+        var x = position[0] * blockSize;
+    }
+
+    function Snake (body) {
+        this.body = body;
+        this.draw = function() {
+            ctx.save();
+            ctx.fillStyle = "#ff0000";
+            for(var i = 0; i < this.body.length; i++){
+                drawBlock(ctx, this.body[i]);
+            };
+            ctx.restore();
+        };
+    }
+    
     
 }
